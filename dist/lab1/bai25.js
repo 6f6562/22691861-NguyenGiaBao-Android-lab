fd58d45513fdf5f16d6bc300be5052167f9c8ccf
6f6562 <datbao124@gmail.com>
2025-08-29 08:50:02 +0700

    change to branch lab2

--- dist/lab1/bai25.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ShapeStatic = void 0;
-class ShapeStatic {
-    static describe() {
-        console.log("shaping");
-    }
-}
-exports.ShapeStatic = ShapeStatic;
